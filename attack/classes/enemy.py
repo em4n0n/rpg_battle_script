9cc7432823ef1376f858bc9a21b065d36dc8574c
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-05-21 23:14:53 -0700

    Update enemy.py
    add init

--- attack/classes/enemy.py
+++ attack/classes/enemy.py
@@ -8,14 +8,15 @@ import random
 #         self.mp = mp
 
 class Enemy:
-    atkl = 60
-    atkh = 80
+    def __init__(self, atkl, atkh): # pass variables/objects in parenthesis
+        self.atkl = atkl
+        self.atkh = atkh
 
     def getAtk(self): # self creates instance
         print(self.atkl) 
 
-enemy1 = Enemy() # instantiates the class to its own obj
+enemy1 = Enemy(40, 49) # instantiates the enemy class to its own obj
 enemy1.getAtk() # calls getAtk
 
-enemy2 = Enemy()
+enemy2 = Enemy(75, 90)
 enemy2.getAtk()
