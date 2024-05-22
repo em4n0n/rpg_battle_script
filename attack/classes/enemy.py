c25de76c449c0d2b2cfc22ea479a14c73d3d816d
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-05-21 23:21:07 -0700

    Update enemy.py
    add Hp

--- attack/classes/enemy.py
+++ attack/classes/enemy.py
@@ -8,15 +8,22 @@ import random
 #         self.mp = mp
 
 class Enemy:
+    hp = 200
+
     def __init__(self, atkl, atkh): # pass variables/objects in parenthesis
         self.atkl = atkl
         self.atkh = atkh
 
     def getAtk(self): # self creates instance
-        print(self.atkl) 
+        print("atk is", self.atkl) 
+
+    def getHp(self):
+        print("Hp is", self.hp)
 
 enemy1 = Enemy(40, 49) # instantiates the enemy class to its own obj
 enemy1.getAtk() # calls getAtk
+enemy1.getHp()
 
 enemy2 = Enemy(75, 90)
 enemy2.getAtk()
+enemy2.getHp()
