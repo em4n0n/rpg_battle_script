937eab28d3b4cd96c2229306d05be67ec365f8c8
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-05-22 23:19:02 -0700

    Update __init__.py
    Importing the Enemy class and using the functions

---
+++ classes/__init__.py
@@ -0,0 +1,5 @@
+from .enemy import Enemy # import Enemy class from enemy.py
+
+enemy = Enemy(200, 60) # assign a variable and input parameters
+print("HP: ", enemy.get_hp()) # use the get_hp function
+
